d96384cac60fc7d9d6f1629d00e1b9e10d0e5cf6
Dorus <30291690+dwrolvink@users.noreply.github.com>
2018-12-31 15:07:02 +0100

    Update deploy.sh

--- i3/deploy.sh
+++ i3/deploy.sh
@@ -45,19 +45,30 @@ pacman -S manjaro-settings-samba 	--noconfirm
 printstep "[Installing user settings]"
 #############################################
 
-# Terminal settings
-unalias cp
-echo "VISUAL=nano; export VISUAL EDITOR=nano; export EDITOR" 	>> /home/dorus/.profile
-echo "URxvt.transparent: true" 					>> /home/dorus/.Xresources
-echo "URxvt.shading:20"						>> /home/dorus/.Xresources
-
 # Create directories
 sudo -u dorus mkdir /home/dorus/nwd
+sudo -u dorus mkdir /home/dorus/std-conf
+
+# Backup standard config files
+cp   /home/dorus/.i3/config		/home/dorus/std-conf/i3-conf
+cp   /home/dorus/.Xresources		/home/dorus/std-conf/.Xresources
 
 # Copy files over
 cp -f config/i3-config 		/home/dorus/.i3/config			# i3 config
 cp    config/mountnwd.service 	/etc/systemd/system/mountnwd.service	# Mount network drive
 cp    config/20-radeon.conf 	/etc/X11/xorg.conf.d/20-radeon.conf 	# anti tearing
+
+# Tweaking
+# Terminal settings
+unalias cp
+echo "VISUAL=nano; export VISUAL EDITOR=nano; export EDITOR" 	>> /home/dorus/.profile
+
+# Terminal design
+echo "URxvt.transparent: true" 					>> /home/dorus/.Xresources
+echo "URxvt.shading: 20"					>> /home/dorus/.Xresources
+echo "URxvt*color12: #f2db32"					>> /home/dorus/.Xresources
+
+
 
 # Mount network drive
 cp -f mountnwd.service /etc/systemd/system/mountnwd.service
