42aeed094f9e67b1aa4c6cccd501d6aaef3d6417
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-01-05 18:48:10 +0100

    Update deploy_konishipcmrhub.sh

--- deploy_konishipcmrhub.sh
+++ deploy_konishipcmrhub.sh
@@ -5,16 +5,22 @@
   
  # Install nginx if not yet installed
  # -----
+ printstep "Installing Nginx..."
+ 
  if [ ! -d "/etc/nginx" ]; then
     . nginx/install_nginx.sh
+ else
+   printstep "Nginx has already been installed, skipping" $NOTIFMSG
  fi
  
  # Create website folders/records
  # -----
+ printstep "Creating nginx website $MAINWEBSITE"
  createNewSite "$MAINWEBSITE"
  
  # Get files from github
  # -----
+ printstep "Getting backend and frontend from github"
  if [ ! -d "$DEVFOLDER" ]; then
    mkdir $DEVFOLDER
  fi
