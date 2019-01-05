e513e40e87e25cbd6938b8de07d422df51f457e2
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-01-05 18:27:04 +0100

    Update deploy_konishipcmrhub.sh

--- deploy_konishipcmrhub.sh
+++ deploy_konishipcmrhub.sh
@@ -15,12 +15,17 @@
  
  # Get files from github
  # -----
+ if [ ! -d "$DEVFOLDER" ]; then
+   mkdir $DEVFOLDER
+ fi
+ 
  # Remove possible old folders
- rm -rf $MAINFOLDER/$FRONTEND
  rm -rf $MAINFOLDER/$BACKEND
+ rm -rf $DEVFOLDER/$FRONTEND 
  
  # Clone 
  cd $MAINFOLDER
  git clone https://github.com/konishi-project/zimmerman.git
+ cd $DEVFOLDER
  git clone https://github.com/konishi-project/higala.git
 
