de10983c0b01a5de05e84805b9bf508085eb4eb8
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-01-05 18:46:30 +0100

    Update _functions.sh

--- _functions.sh
+++ _functions.sh
@@ -1,13 +1,22 @@
+ERRORMSG=1
+NOTIFMSG=2
+
 printstep(){
         BLUE='\033[0;34m'
+        YELLOW='\033[0;33m'
         REEE='\033[0;31m'
         NC='\033[0m' # No Color
 
         ERROR=1
+        NOTIFICATION=2
 
-        if [[ $2 -eq $ERROR ]];
+        if [[ $2 -eq $ERROR ]]; 
         then
               echo -e "${REEE} ${1} ${NC}"
+              
+        elif [[ $2 -eq $NOTIFICATION ]];
+        then
+              echo -e "${YELLOW} ${1} ${NC}"
         else
               echo -e "${BLUE} ${1} ${NC}"
         fi
