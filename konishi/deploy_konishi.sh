4cacdba3d4118c6e56e0525aa1e2cac9341e72a7
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-01-05 20:33:40 +0100

    Update deploy_konishi.sh

--- konishi/deploy_konishi.sh
+++ konishi/deploy_konishi.sh
@@ -1,37 +1,96 @@
 #!/bin/bash
+
  . _functions.sh
  . _gitautomata_config.sh
- . nginx/new_site.sh
+ 
+ . ../nginx/nginx.sh
+ . ../nginx/new_site.sh 
   
- # Install nginx if not yet installed
- # -----
+ ##############################################
+ #               SET UP FOLDERS               #
+ ##############################################  
+ # Create main folder (/var/www/) if is doesn't
+ if [ ! -d "$MAINFOLDER" ]; then
+   mkdir $MAINFOLDER
+ fi 
+ 
+  # Create dev folder (/var/www/dev/) if it existn't
+ if [ ! -d "$DEVFOLDER" ]; then
+   mkdir $DEVFOLDER
+ fi 
+ 
+ # Create config backup directories
+ mkdir $GITAUTOMATA/$BACKEND
+ mkdir $GITAUTOMATA/$FRONTEND
+  
+ ##############################################
+ #            INSTALL REQUIREMENTS            #
+ ##############################################
+ printstep "Installing requirements"
+ 
+ sudo apt-get update
+ sudo apt-get install python3-pip --yes
+ sudo apt-get install virtualenv --yes
+ sudo apt-get install sqlite --yes
+ 
+ ##############################################
+ #         INSTALL & CONFIGURE NGINX          #
+ ##############################################
  printstep "Installing Nginx..."
  
+ # Install nginx if not yet installed
  if [ ! -d "/etc/nginx" ]; then
-    . nginx/install_nginx.sh
+    InstallNginx $MAINUSER
  else
    printstep "Nginx has already been installed, skipping" $NOTIFMSG
  fi
  
- # Create website folders/records
- # -----
+ # Create website folders & records
  printstep "Creating nginx website $MAINWEBSITE"
  createNewSite "$MAINWEBSITE"
  
- # Get files from github
- # -----
+ ##############################################
+ #     GET BACKEND & FRONTEND FROM GITHUB     #
+ ##############################################
  printstep "Getting backend and frontend from github"
- if [ ! -d "$DEVFOLDER" ]; then
-   mkdir $DEVFOLDER
- fi
  
- # Remove possible old folders
- rm -rf $MAINFOLDER/$BACKEND
- rm -rf $DEVFOLDER/$FRONTEND 
+ # Remove old folders if they exist
+ rm -rf $BACKENDFOLDER
+ rm -rf $FRONTENDFOLDER
  
- # Clone 
+ # Get data from github
  cd $MAINFOLDER
  git clone https://github.com/konishi-project/zimmerman.git
+ 
  cd $DEVFOLDER
  git clone https://github.com/konishi-project/higala.git
+ 
+ ##############################################
+ #               SETUP ZIMMERMAN              #
+ ##############################################
+ printstep "Setting up zimmerman"
+ cd $BACKENDFOLDER
+ 
+ printstep "STOP... HAMMERTIME!" $ERRORMSG
+ printstep "Open a new SSH window, and do the following:" $NOTIFMSG
+ printstep "- Set SQLALCHEMY_DATABASE_URI='sqlite:////${BACKENDFOLDER}/konishi.db' in /${BACKENDFOLDER}/config.py" $NOTIFMSG
+ read -rsp $'Press any key to continue when you\'re done...\n' -n1 key
+
+ # Copy config.py over to a backup folder, so it will be restored after doing 'git reset' in freshpull.sh
+ cp $BACKENDFOLDER/config.py $GITAUTOMATA/$BACKEND/config.py
+ 
+ # Copy db installation script over so it can find app.py
+ cp $GITAUTOMATA/zimmerman_installdb.py $BACKENDFOLDER/zimmerman_installdb.py 
+  
+ # Create venv, and install venv requirements
+ virtualenv konishienv
+ source konishienv/bin/activate
+ pip3 install -r requirements.txt
+ 
+ # Create database
+ python3 $BACKENDFOLDER/zimmerman_installdb.py
+ 
+ ##############################################
+ #                 SETUP HIGALA               #
+ ##############################################
 
