fd8c6fa23e992b93e3965a8891779653b2b9cfe1
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-01-05 19:40:14 +0100

    Update zimmerman_installdb.py

--- konishi/zimmerman_installdb.py
+++ konishi/zimmerman_installdb.py
@@ -1,3 +1,3 @@
-from app import db # Import the database object from app.py
-db.create_all() # Create the database cluster
-quit() # Exit out of python
+from app import db  #Import the database object from app.py
+db.create_all()     # Create the database cluster
+quit()              # Exit out of python
