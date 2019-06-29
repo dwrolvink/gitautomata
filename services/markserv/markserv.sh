99ee1ba55a4fed25f929dedeb50ab5a343bade2d
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-06-29 17:03:38 +0200

    Update markserv.sh

--- services/markserv/markserv.sh
+++ services/markserv/markserv.sh
@@ -1,36 +1,39 @@
 # http://www.dwrolvink.com/?view=coding/website/install_markserv
+. ./config.sh
 
 # :: INSTAL NODEJS ------------------------------------
-
 # Add node repo, install make tools
-yum install -y gcc-c++ make
+sudo yum install -y gcc-c++ make
 curl -sL https://rpm.nodesource.com/setup_11.x | sudo -E bash -
 
 # Install nodejs
 sudo yum install -y nodejs
 
 # Install yarn
- curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
- sudo yum install -y yarn
+curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+sudo yum install -y yarn
  
  
  # :: SET FIREWALL ------------------------------------
- 
-firewall-cmd --zone=public --add-port=8081/tcp --permanent
-firewall-cmd --reload
+sudo firewall-cmd --zone=public --add-port=${SERVICE_PORT}/tcp --permanent
+sudo firewall-cmd --reload
 
 
 # :: INSTALL MARKSERV ---------------------------------
-
 # Install markserv
 yarn global add markserv
 
 # Edit template (manual)
-vi /usr/local/share/.config/yarn/global/node_modules/markserv/lib/templates/markdown.html
+vi /home/${USER}/.yarn/bin/markserv/lib/templates/markdown.html
 
 
 # :: ENABLE MARKSERV ---------------------------------
+cp markserver.service-master markserver.service
+sed -i "s+{{WEBSITE_NAME}}+${WEBSITE_NAME}+g" markserver.service
+sed -i "s+{{WEBSITE_DIRECTORY}}+${WEBSITE_DIRECTORY}+g" markserver.service
+sed -i "s+{{SERVICE_PORT}}+${SERVICE_PORT}+g" markserver.service
+sed -i "s+{{USER}}+${USER}+g" markserver.service
+sudo cp -f markserver.service /etc/systemd/system/markserver.service
 
-cp -f markserver.service-master /etc/systemd/system/markserver.service
-systemctl start markserver
-systemctl enable markserver
+sudo systemctl start markserver
+sudo systemctl enable markserver
