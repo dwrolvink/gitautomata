afcb1e9c68c3d606261877ebc89c0e1c5e997811
Dorus <30291690+dwrolvink@users.noreply.github.com>
2019-06-29 16:10:18 +0200

    Create TigerVNC.sh

---
+++ i3_home/modules/TigerVNC/TigerVNC.sh
@@ -0,0 +1,13 @@
+# Install TigerVNC 
+pacman -S tigervnc --noconfirm
+
+# Fill in template
+cp tigervnc.service-master tigervnc.service
+sed -i "s+{{USER}}+${USER}+g" tigervnc.service
+
+# Copy service file over
+cp -f tigervnc.service /etc/systemd/system/tigervnc.service
+
+# Enable TigerVNC
+systemctl enable tigervnc.service
+systemctl start tigervnc.service
