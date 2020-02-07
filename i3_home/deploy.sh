d205dec0a0c7c69a0768e33ba723fd5e0f5297ee
Dorus <30291690+dwrolvink@users.noreply.github.com>
2020-02-07 17:50:05 +0100

    Update deploy.sh

--- i3_home/deploy.sh
+++ i3_home/deploy.sh
@@ -28,15 +28,16 @@ pacman -Syuu
 # ---------------------------------------------------
 # Install AUR packages\
 pacman -S 		yay 			--noconfirm --needed
-# Misc shell functions (for sponge mostly)
+# Misc shell functions
 pacman -S 		moreutils		--noconfirm --needed
+pacman -S		xclip			--noconfirm --needed
 # Check keyboard and mouse commands
 pacman -S 		xev			--noconfirm --needed
 # Utilities
 pacman -S		flameshot		--noconfirm --needed # screenshots
 pacman -S		redshift		--noconfirm --needed # f.lux for linux
 
-# SSH
+vv# SSH
 # ---------------------------------------------------
 # SSH is only turned on by default for VM's
 pacman -S openssh				--noconfirm --needed
@@ -50,19 +51,20 @@ fi
 # Main applications
 # ---------------------------------------------------
 # Browsers
-#pacman -S 		chromium 		--noconfirm --needed
+pacman -S 		chromium 		--noconfirm --needed
 pacman -S 		firefox 		--noconfirm --needed
-# Audio
-#sudo -u $USER 	yay 	spotify		--noconfirm --needed
+
 # Folder browsing	
 pacman -S 		pcmanfm 		--noconfirm --needed
+
 # Editors
 pacman -S 		code			--noconfirm --needed
-# Virtualization
-#if [ "$MACHINE_TYPE"=="BARE_METAL" ];
-#then
-#	pacman -S 		virtualbox		--noconfirm --needed
-#fi
+
+
+if [ "$MACHINE_TYPE"=="BARE_METAL" ];
+then
+	sudo -u $USER 	yay 	spotify		--noconfirm --needed
+fi
 
 # Theming
 # ---------------------------------------------------
@@ -96,12 +98,12 @@ cp   /home/${USER}/.Xresources		/home/${USER}/std-conf/.Xresources
 
 # Copy files over
 # ---------------------------------------------------
-cp -f config/i3-config 			/home/${USER}/.i3/config			# i3 config
+cp -f config/i3-config 			/home/${USER}/.i3/config		# i3 config
 cp -f /etc/i3status.conf 		/home/${USER}/.i3status.conf		# Set user version of i3 status bar config
-cp -f config/.xbindkeysrc		/home/${USER}/.xbindkeysrc			# xbindkeys config
-cp -f src/wallpaper.jpg			/home/${USER}/Pictures/Wallpapers/  # wallpaper
+cp -f config/.xbindkeysrc		/home/${USER}/.xbindkeysrc		# xbindkeys config
 cp -f config/i3-config 			/home/${USER}/.config/
 cp -f config/redshift.conf		/home/${USER}/.config/redshift/redshift.conf
+cp -f src/wallpaper.jpg			/home/${USER}/Pictures/Wallpapers/  	# wallpaper
 
 # Tweaking
 # ---------------------------------------------------
@@ -116,15 +118,18 @@ else
 fi
 
 # Remove cp safety
-# sudo -u $USER unalias cp # doesn't work!
+unalias cp 
 
 # Set Nano as standard editor
 echo "VISUAL=nano; export VISUAL EDITOR=nano; export EDITOR" 	>> /home/${USER}/.profile
 
 # Terminal design
 echo "URxvt.transparent: true" 					>> /home/${USER}/.Xresources
-echo "URxvt.shading: 20"						>> /home/${USER}/.Xresources
+echo "URxvt.shading: 20"					>> /home/${USER}/.Xresources
 echo "URxvt*color12: #f2db32"					>> /home/${USER}/.Xresources
+
+# Append to .xinitrc
+echo "sxhkd -f 100 &"						>> /home/${USER}/.xinitrc
 
 # Set lan: 192..." color in i3 status bar to #f2db32
 cat /home/${USER}/.i3status.conf | awk '{gsub(/color_good = "#[a-zA-Z0-9]{6}"/,"color_good = \"#f2db32\"")}1' | sponge /home/${USER}/.i3status.conf
@@ -135,9 +140,7 @@ cat /home/${USER}/.i3status.conf | awk '{gsub(/color_good = "#[a-zA-Z0-9]{6}"/,"
 cd modules/NetworkDrive
 . ./NetworkDrive.sh
 
-cd ..
-cd Russian
-. ./Russian.sh
+
 # ---------------------------------------------------
 
 # Restart i3
